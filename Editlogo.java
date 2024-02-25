999a412b31951ece2190bccb4eafe92505726390
AkshaySingh240 <160828308+AkshaySingh240@users.noreply.github.com>
2024-02-25 16:34:00 +0530

    Add files via upload

--- Editlogo.java
+++ Editlogo.java
@@ -8,19 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Editlogo {
 
 	public static void main(String[] args) {
-	 
+		System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
 		String baseURL = "https://dev.klaarhq.com";
         WebDriver driver = new ChromeDriver();
         
-        WebElement usernameInput = driver.findElement(By.id("username-field")).sendKeys("deepa.nayak@gamma.klaar.team");
-        WebElement passwordInput = driver.findElement(By.id("password-field")).sendKeys("Klaar2021"); 
-        WebElement loginButton = driver.findElement(By.id("loginButton")).loginButton.click();
         
         
-        driver.findElement(By.xpath("//button[@data-cy='profile-nav-menu-button']"))
+        WebElement usernameInput = driver.findElement(By.xpath("//input[@id='email-field']")).sendKeys("deepa.nayak@gamma.klaar.team");
+        WebElement passwordInput = driver.findElement(By.xpath("//input[@id='password-field']")).sendKeys("Klaar2021"); 
+        WebElement loginButton = driver.findElement(By.xpath("//button[@id='login-btn']")).login-btn.click();
+        
+        
+        driver.findElement(By.xpath("//button[@data-cy='settings-nav-menu-button']"))
         driver.findElement(By.xpath("//a[@href='/settings/workspace/details']")).click(); 
         
-        WebElement chooseFilesButton = driver.findElement(By.xpath("//input[@type='file']")); 
+        WebElement chooseFilesButton = driver.findElement(By.xpath("//div[@class='ant-upload ant-upload-btn']")); 
         chooseFilesButton.sendKeys("C:\\Users\\hp\\Desktop\\Solar.png");        
         
         WebElement saveButton = driver.findElement(By.xpath("//button[@data-cy='settings-workspace-logo-save-upload-button']"));
